ae45c6b6c5835bc73a0f5ce35438e710d1f5ca43
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2026-09-19 20:34:39 -0600

    add practica 1

--- Practicas/presentacionPersonal.php
+++ Practicas/presentacionPersonal.php
@@ -1,2 +1,19 @@
 <?php
 
+/*
+1.	Presentación personal 
+Declara variables para guardar:
+•	Nombre. 
+•	Edad. 
+•	Estatura. 
+•	Si es estudiante. 
+Muestra una oración con todos los datos usando echo.
+Conceptos: variables, tipos de datos, concatenación e interpolación.
+*/
+
+$nombre = "Dali";
+$edad = 25;
+$estatura = 1.82;
+$esEstudiante = true;
+
+echo "Hola, mi nombre es $nombre, tengo $edad años de edad, mido $estatura metros y " . ($esEstudiante ? "soy estudiante." : "no soy estudiante.");
