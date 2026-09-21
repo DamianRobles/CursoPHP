4caeeb259409f15102e59481a96204fb218378a2
Dalí Robles <49328533+DamianRobles@users.noreply.github.com>
2026-09-21 17:09:56 -0600

    Update inventarioArreglos.php

--- Practicas/inventarioArreglos.php
+++ Practicas/inventarioArreglos.php
@@ -13,3 +13,48 @@ Recorre el arreglo y muestra:
 Conceptos: arreglos multidimensionales, foreach, operadores y comparaciones.
 */
 
+$botana = [
+    [
+        "Nombre" => "Doritos",
+        "Cantidad" => 55,
+        "Precio" => 20
+    ],
+    [
+        "Nombre" => "Prispas",
+        "Cantidad" => 15,
+        "Precio" => 16
+    ],
+    [
+        "Nombre" => "Cacahuates",
+        "Cantidad" => 30,
+        "Precio" => 25
+    ],
+    [
+        "Nombre" => "Takis",
+        "Cantidad" => 20,
+        "Precio" => 18
+    ],
+    
+];
+
+$totalProducto = 0;
+$totalInventario = 0;
+$nombreProductoMayorValor = "";
+$mayorValor = 0;
+
+foreach($botana as $producto){
+    $valorProducto = $producto["Cantidad"] * $producto["Precio"];
+    $totalInventario += $valorProducto;
+    echo "Total del producto \"{$producto['Nombre']}\" es: \${$valorProducto}";
+    echo "<br>";
+    if($valorProducto > $mayorValor){
+        $mayorValor = $valorProducto;
+        $nombreProductoMayorValor = $producto["Nombre"];
+    }
+
+}
+echo "<br>";
+echo "Total del inventario es: $totalInventario";
+echo "<br>";
+echo "<br>";
+echo "Producto con mayor valor acumulado es: \"$nombreProductoMayorValor\" con un valor de: \${$mayorValor}";
